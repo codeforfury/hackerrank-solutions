432d7f0d787c1062d003a414a94e40b45745a5c3
Rajiv Das <dasrajiv6507@gmail.com>
2026-09-03 23:36:51 +0530

    Delete sql/binary-search-tree-1 directory

--- sql/binary-search-tree-1/solution.sql
+++
@@ -1,8 +0,0 @@
-SELECT N,
-       CASE 
-           WHEN P IS NULL THEN 'Root'
-           WHEN N IN (SELECT P FROM BST WHERE P IS NOT NULL) THEN 'Inner'
-           ELSE 'Leaf'
-       END
-FROM BST
-ORDER BY N;
